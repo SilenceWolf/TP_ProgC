817022b613174f950b7bbc70a3830fa16ec0e927
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 11:53:34 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -5,17 +5,17 @@ int main(){
   int i;
   int j;
   int taille;
-  printf("la taille du triangle: ");
+  
   scanf("%d", &taille);
   
   for (i = 1; i <= taille; i++) {
         for (j = 1; j <= i; j++) {
 
-            // Première et dernière ligne → que des *
+           
             if (i == 1 || i == taille) {
                 printf("* ");
             }
-            // Sinon : * aux bords, # à l'intérieur
+            
             else if (j == 1 || j == i) {
                 printf("* ");
             } else {
@@ -26,6 +26,7 @@ int main(){
     }
 
 }
+
 
 
 
