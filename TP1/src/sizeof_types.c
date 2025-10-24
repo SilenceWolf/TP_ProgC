3a41167a38b17bda11ed7b771531b9af4c26a02f
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 10:13:52 +0200

    Add unsigned types and their sizes to sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -2,20 +2,35 @@
 #include <string.h>
 
 int main(){
+  
   char var_char;
   short var_short;
+  
   int var_int;
+  unsigned int var_int_unsigned;
+  
   long int var_long_int;
+  unsigned long int var_long_int_unsigned;
+  
   long long int var_long_long_int;
+  unsigned long long int var_long_long_int_unsigned;
+  
   float var_float;
   double var_double;
   long double var_long_double;
 
   printf("var char size :" ,size(var_char));
   printf("var short size :" ,size(var_short));
+  
   printf("var int size :" ,size(var_int));
+  printf("var int unsigned size :" ,size(var_int_unsigned));
+  
   printf("var long int size :" ,size(var_long_int));
+  printf("var long int unsigned size :" ,size(var_long_int_unsigned));
+  
   printf("var long long int size :" ,size(var_long_long_int));
+  printf("var long long int unsigned size :" ,size(var_long_long_int_unsigned));
+  
   printf("var float size :" ,size(var_float));
   printf("var double size :" ,size(var_double));
   printf("var long size :" ,size(var_long_double));
@@ -25,4 +40,5 @@ int main(){
 
   
 }
+
 
