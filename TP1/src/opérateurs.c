a3c55eaf88e84461d8d6bcc30a88c48b5c641385
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 11:07:18 +0200

    Fix printf format specifiers in opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -9,16 +9,17 @@ int b = 3;
 
 int main(){
   
-  printf("addition : %f", a+b);
-  printf("soustraction : %f",a-b);
-  printf("multiplication : %f",a*b);
-  printf("division : %f",a/b);
-  printf("modulo : %f", a%b );
+  printf("addition : %d", a+b);
+  printf("soustraction : %d",a-b);
+  printf("multiplication : %d",a*b);
+  printf("division : %d",a/b);
+  printf("modulo : %d", a%b );
   printf("égale : %d",a==b);
   printf("supérieur : %d",a>b);
 
 
 }
+
 
 
 
