083b3a2992a238be7330725b9e2889a191f4505c
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 10:05:45 +0200

    Add sizeof_types.c to demonstrate data types sizes

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char var_char;
+  short var_short;
+  int var_int;
+  long int var_long_int;
+  long long int var_long_long_int;
+  float var_float;
+  double var_double;
+  long double var_long_double;
+}
