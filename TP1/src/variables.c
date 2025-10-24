94b7a86091dea509928695dfc6e16ac00ca9d449
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 10:34:07 +0200

    Refactor variable sizes and fix data types

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -12,11 +12,11 @@ int main(){
   int var_int=1620;
   unsigned int var_int_unsigned=-1620;
   
-  long int var_long_int=888888888888888888;
-  unsigned long int var_long_int_unsigned=-8888888888888888888888888;
+  long int var_long_int=88888888888;
+  unsigned long int var_long_int_unsigned=-8888888888888888;
   
-  long long int var_long_long_int=9999999999999999999999999999999999999999999999999999;
-  unsigned long long int var_long_long_int_unsigned=-999999999999999999999999999999999999999999999;
+  long long int var_long_long_int=9999999999999999999999999;
+  unsigned long long int var_long_long_int_unsigned=-9999999999999999999999;
   
   float var_float=0.1;
   double var_double=0.1254;
@@ -25,17 +25,17 @@ int main(){
   printf("var char size : %c\n" ,sizeof(var_char));
   printf("var char unsigned size : %c\n" ,sizeof(var_char_unsigned));
   
-  printf("var short size : %d\n" ,sizeof(var_short));
-  printf("var short unsigned size : %zu octet(s)\n" ,sizeof(var_short_unsigned));
+  printf("var short size : %i\n" ,sizeof(var_short));
+  printf("var short unsigned size : %i\n" ,sizeof(var_short_unsigned));
   
-  printf("var int size : %d\n" ,sizeof(var_int));
-  printf("var int unsigned size : %d\n" ,sizeof(var_int_unsigned));
+  printf("var int size : %i\n" ,sizeof(var_int));
+  printf("var int unsigned size : %i\n" ,sizeof(var_int_unsigned));
   
-  printf("var long int size : %d\n" ,sizeof(var_long_int));
-  printf("var long int unsigned size : %d\n" ,sizeof(var_long_int_unsigned));
+  printf("var long int size : %i\n" ,sizeof(var_long_int));
+  printf("var long int unsigned size : %i\n" ,sizeof(var_long_int_unsigned));
   
-  printf("var long long int size : %d\n" ,sizeof(var_long_long_int));
-  printf("var long long int unsigned size : %d\n" ,sizeof(var_long_long_int_unsigned));
+  printf("var long long int size : %i\n" ,sizeof(var_long_long_int));
+  printf("var long long int unsigned size : %i\n" ,sizeof(var_long_long_int_unsigned));
   
   printf("var float size : %f\n" ,sizeof(var_float));
   printf("var double size : %f\n" ,sizeof(var_double));
@@ -44,3 +44,4 @@ int main(){
   
   
 }
+
