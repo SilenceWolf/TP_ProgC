1a4f87faec63541c2adfd76fa82d18a0d173289e
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 10:20:31 +0200

    Fix size function calls to sizeof operator

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -21,29 +21,30 @@ int main(){
   double var_double;
   long double var_long_double;
 
-  printf("var char size :" ,size(var_char));
+  printf("var char size :" ,sizeof(var_char));
   
-  printf("var short size :" ,size(var_short));
-  printf("var short unsigned size :" ,size(var_short_unsigned));
+  printf("var short size :" ,sizeof(var_short));
+  printf("var short unsigned size :" ,sizeof(var_short_unsigned));
   
-  printf("var int size :" ,size(var_int));
-  printf("var int unsigned size :" ,size(var_int_unsigned));
+  printf("var int size :" ,sizeof(var_int));
+  printf("var int unsigned size :" ,sizeof(var_int_unsigned));
   
-  printf("var long int size :" ,size(var_long_int));
-  printf("var long int unsigned size :" ,size(var_long_int_unsigned));
+  printf("var long int size :" ,sizeof(var_long_int));
+  printf("var long int unsigned size :" ,sizeof(var_long_int_unsigned));
   
-  printf("var long long int size :" ,size(var_long_long_int));
-  printf("var long long int unsigned size :" ,size(var_long_long_int_unsigned));
+  printf("var long long int size :" ,sizeof(var_long_long_int));
+  printf("var long long int unsigned size :" ,sizeof(var_long_long_int_unsigned));
   
-  printf("var float size :" ,size(var_float));
-  printf("var double size :" ,size(var_double));
-  printf("var long size :" ,size(var_long_double));
+  printf("var float size :" ,sizeof(var_float));
+  printf("var double size :" ,sizeof(var_double));
+  printf("var long size :" ,sizeof(var_long_double));
   
   
 
 
   
 }
+
 
 
 
