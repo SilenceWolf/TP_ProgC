5c1286f2a95d7dea68b79351aa4e947a1a75b062
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 10:07:53 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -10,4 +10,19 @@ int main(){
   float var_float;
   double var_double;
   long double var_long_double;
+
+  printf("var char size :" ,size(var_char));
+  printf("var short size :" ,size(var_short));
+  printf("var int size :" ,size(var_int));
+  printf("var long int size :" ,size(var_long_int));
+  printf("var long long int size :" ,size(var_long_long_int));
+  printf("var float size :" ,size(var_float));
+  printf("var double size :" ,size(var_double));
+  printf("var long size :" ,size(var_long_double));
+  
+  
+
+
+  
 }
+
