8b9339d683ef922da38cc4b62c45602ea6058003
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 11:31:12 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,14 +4,17 @@
 int main(){
   int i;
   int j;
+  int taille;
+  scanf("%i", &taille);
+  
   for(i=0;i<10;i++){
     
     for(j=1;j<i-1;j++){
       
-      if (j==i) {
-        printf("#");
-      } else{
+      if (j%2==1 || i==0 || i==taille) {
         printf("*");
+      } else{
+        printf("#");
       }
 
     }
@@ -20,6 +23,7 @@ int main(){
 
 
 }
+
 
 
 
