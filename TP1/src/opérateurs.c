773323c1d41ed7245fdcc154478639671c57da83
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 11:03:57 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,3 +1,22 @@
-a = 16
+#include <stdio.h>
+#include <string.h>
 
-ii. b = 3
+
+float a = 16;
+float b = 3;
+
+
+
+int main(){
+  
+  printf("addition : %f", a+b);
+  printf("soustraction : %f",a-b);
+  printf("multiplication : %f",a*b);
+  printf("division : %f",a/b);
+  printf("modulo : %f", a%b );
+  printf("égale : %f",a==b);
+  printf("supérieur : %f",a>b);
+
+
+}
+
