13be17dbecb954808880893bd63fd45d4679b1a3
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-10-24 11:08:51 +0200

    Format output with newlines in opérateurs.c
    Added newline characters to printf statements for better output formatting.

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -9,16 +9,17 @@ int b = 3;
 
 int main(){
   
-  printf("addition : %d", a+b);
-  printf("soustraction : %d",a-b);
-  printf("multiplication : %d",a*b);
-  printf("division : %d",a/b);
-  printf("modulo : %d", a%b );
-  printf("égale : %d",a==b);
-  printf("supérieur : %d",a>b);
+  printf("addition : %d\n", a+b);
+  printf("soustraction : %d\n",a-b);
+  printf("multiplication : %d\n",a*b);
+  printf("division : %d\n",a/b);
+  printf("modulo : %d\n", a%b );
+  printf("égale : %d\n",a==b);
+  printf("supérieur : %d\n",a>b);
 
 
 }
+
 
 
 
