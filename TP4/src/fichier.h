50d0a6a490a1f66ab7b07ae208e802dbf9c92948
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-12-02 11:27:37 +0100

    Update fichier.h

--- TP4/src/fichier.h
+++ TP4/src/fichier.h
@@ -1,31 +1,9 @@
 #ifndef FICHIER_H
 #define FICHIER_H
-#include <stdio.h>
-#include <string.h>
 
-void lire_fichier(const char *nom_de_fichier) {
-    FILE *f = fopen(nom_de_fichier, "r");
-    if (!f) {
-        printf("Erreur : impossible d'ouvrir le fichier.\n");
-        return;
-    }
-    char ligne[256];
-    while (fgets(ligne, sizeof(ligne), f)) {
-        printf("%s", ligne);
-    }
-    fclose(f);
-}
-
-void ecrire_dans_fichier(const char *nom_de_fichier, const char *message) {
-    FILE *f = fopen(nom_de_fichier, "a");
-    if (!f) {
-        printf("Erreur : impossible d'ouvrir le fichier.\n");
-        return;
-    }
-    fprintf(f, "%s", message);
-    fclose(f);
-    printf("Message écrit dans %s\n", nom_de_fichier);
-}
+void lire_fichier(const char *nom_de_fichier);
+void ecrire_dans_fichier(const char *nom_de_fichier, const char *message);
 
 #endif
+
 
