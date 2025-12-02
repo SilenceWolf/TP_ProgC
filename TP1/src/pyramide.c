4e90e6b4a3e1557c93fa8b65a86339cf2f2ab6ee
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-12-02 09:24:50 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
 
+
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+
+        // espaces
+        for (int j = 0; j < n - i; j++) {
+            printf(" ");
+        }
+
+        // nombres croissants
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // nombres décroissants
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Pyramide terminée.\n");
+
+    return 0;
+}
