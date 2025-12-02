0d95900d66ecd2f0c89300e1e9d48ac92151d732
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-12-02 11:31:09 +0100

    Update liste.h

--- TP4/src/liste.h
+++ TP4/src/liste.h
@@ -1,25 +1,17 @@
 #ifndef LISTE_H
 #define LISTE_H
-#include <stdio.h>
-#include <string.h>
 
+struct couleur {
+    unsigned char r, g, b, a;
+    struct couleur *suivant;
+};
 
-void init_liste(struct liste_couleurs *liste) {
-    liste->tete = NULL;
-}
+struct liste_couleurs {
+    struct couleur *tete;
+};
 
-void insertion(struct couleur *c, struct liste_couleurs *liste) {
-    c->suivant = liste->tete;
-    liste->tete = c;
-}
-
-void parcours(struct liste_couleurs *liste) {
-    struct couleur *courant = liste->tete;
-    while (courant) {
-        printf("Couleur RGBA : %02X %02X %02X %02X\n", courant->r, courant->g, courant->b, courant->a);
-        courant = courant->suivant;
-    }
-}
+void init_liste(struct liste_couleurs *liste);
+void insertion(struct couleur *c, struct liste_couleurs *liste);
+void parcours(struct liste_couleurs *liste);
 
 #endif
-
