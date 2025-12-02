d8ec461f45c531ba46233d00f6165f8e45fb2642
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-12-02 09:30:12 +0100

    Clean up comments in pyramide.c
    Removed comments for better readability.

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -7,17 +7,17 @@ int main() {
 
     for (int i = 1; i <= n; i++) {
 
-        // espaces
+        
         for (int j = 0; j < n - i; j++) {
             printf(" ");
         }
 
-        // nombres croissants
+        
         for (int j = 1; j <= i; j++) {
             printf("%d", j);
         }
 
-        // nombres décroissants
+        
         for (int j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
