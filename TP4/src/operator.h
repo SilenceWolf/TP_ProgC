9c3da3e1f3d6a817a7330d174b9888b2d15f6181
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-12-02 10:55:27 +0100

    Implement basic arithmetic and bitwise operations
    Added multiple arithmetic and bitwise operation functions.

--- TP4/src/operator.h
+++ TP4/src/operator.h
@@ -1,7 +1,15 @@
 #ifndef OPERATOR_H
 #define OPERATOR_H
 
-// Fonction unique qui fait toutes les opérations
-int calcul(int num1, int num2, char op);
+
+int somme(int a, int b) { return a + b; }
+int difference(int a, int b) { return a - b; }
+int produit(int a, int b) { return a * b; }
+int quotient(int a, int b) { return a / b; }
+int modulo(int a, int b) { return a % b; }
+int et(int a, int b) { return a & b; }
+int ou(int a, int b) { return a | b; }
+int negation(int a) { return ~a; }
 
 #endif
+
