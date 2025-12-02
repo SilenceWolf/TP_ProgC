c9bd040d09a65e079c9450efd5e5ffa6f3e90ca9
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2025-12-02 10:26:28 +0100

    Add phrase search functionality in chercher2.c

---
+++ TP3/src/chercher2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int equals(char *a, char *b) {
+    int i = 0;
+    while (a[i] && b[i]) {
+        if (a[i] != b[i])
+            return 0;
+        i++;
+    }
+    return a[i] == b[i];
+}
+
+int main() {
+    char phrases[10][200] = {
+        "Bonjour, comment ca va ?",
+        "Le temps est magnifique aujourd'hui.",
+        "C'est une belle journee.",
+        "La programmation en C est amusante.",
+        "Les tableaux en C sont puissants.",
+        "Les pointeurs en C peuvent etre deroutants.",
+        "Il fait beau dehors.",
+        "La recherche dans un tableau est interessante.",
+        "Les structures de donnees sont importantes.",
+        "Programmer en C, c'est genial."
+    };
+
+    char recherche[200];
+    printf("Entrez la phrase a chercher : ");
+    fgets(recherche, 200, stdin);
+
+    int found = 0;
+    for (int i = 0; i < 10; i++)
+        if (equals(phrases[i], recherche))
+            found = 1;
+
+    if (found)
+        printf("Phrase trouvee\n");
+    else
+        printf("Phrase non trouvee\n");
+
+    return 0;
+}
